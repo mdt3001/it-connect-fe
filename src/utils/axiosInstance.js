f8a85ad0e3621d187938b1962b2e1b6e3cf0552e
mdt3001 <22521413@gm.uit.edu.vn>
2025-11-15 01:36:16 +0700

    fix Base url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { BASE_URL } from './apiPaths';
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
